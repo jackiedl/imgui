979115f00d72e5659149b0dc29945bf0a1b49116
Jackie <luujackie98@gmail.com>
2025-07-17 11:55:01 -0700

    Add premake

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+        "imgui_demo.cpp",
+		"imgui_internal.h",
+        "imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.cpp"
+	}
+
+  	includedirs {
+		".",
+		"backends",
+		"../GLFW/include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "LM_DEBUG"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		defines "LM_RELEASE"
+		runtime "Release"
+		optimize "on"
